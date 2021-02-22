add85e9c95eb9c952011bff05b04d145b792fee4
stigglesworth3 <Kyle@Stigler.com>
2021-02-22 10:25:42 -0500

    added second screen and text, not linked to first yet

--- app/src/main/java/com/stigler/cs441_proj2/MainActivity.java
+++ app/src/main/java/com/stigler/cs441_proj2/MainActivity.java
@@ -2,13 +2,28 @@ package com.stigler.cs441_proj2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView welcomeText;
+    Button screenButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screenButton = (Button) findViewById(R.id.screenButton);
+    }
+
+    public void switchScreens (View view)
+    {
+        //Intent actAction = new Intent(this,);
     }
 }
