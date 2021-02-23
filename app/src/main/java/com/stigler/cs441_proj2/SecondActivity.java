3087db96e216e9d4ba5e7bc4abd4cb6e2756d73e
stigglesworth3 <Kyle@Stigler.com>
2021-02-23 12:50:48 -0500

    added button to switch back to first screen

--- app/src/main/java/com/stigler/cs441_proj2/SecondActivity.java
+++ app/src/main/java/com/stigler/cs441_proj2/SecondActivity.java
@@ -2,13 +2,28 @@ package com.stigler.cs441_proj2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity
+{
+
+    Button backButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        backButton = (Button) findViewById(R.id.backButton);
+    }
+
+    public void goBack(View view)
+    {
+        Intent actAction = new Intent(this, MainActivity.class);
+        startActivity(actAction);
     }
 }
